0b7bbfe3ab5da8299e154d7a92b222d29861b81d
Drilon Thaqi <ddrilonthaci@gmail.com>
2024-07-13 15:23:25 +0200

    Hero section and both forms with routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import LoginView from '@/views/forms/LoginView.vue'
+import RegisterView from '@/views/forms/RegisterView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +24,16 @@ const router = createRouter({
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             // component: () => import('../views/AboutView.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView
         }
     ]
 })
